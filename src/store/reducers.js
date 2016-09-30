05affecc302a91b814c33052d7fb51cf2d23651b
Iurii Kobernyk <koiu@ciklum.com>
2016-09-30 08:45:36 +0300

    refactoring

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,17 +1,19 @@
-import { combineReducers } from 'redux'
-import { routerReducer as router } from 'react-router-redux'
+import {combineReducers} from 'redux'
+import {routerReducer as router} from 'react-router-redux'
+import demo from '../reducers/demo.reducer';
 
 export const makeRootReducer = (asyncReducers) => {
-  return combineReducers({
-    // Add sync reducers here
-    router,
-    ...asyncReducers
-  })
-}
+	return combineReducers({
+		// Add sync reducers here
+		router,
+		demo,
+		...asyncReducers
+	})
+};
 
 export const injectReducer = (store, { key, reducer }) => {
-  store.asyncReducers[key] = reducer
-  store.replaceReducer(makeRootReducer(store.asyncReducers))
-}
+	store.asyncReducers[key] = reducer
+	store.replaceReducer(makeRootReducer(store.asyncReducers))
+};
 
 export default makeRootReducer
